26e8338cc2e67777bb2e6fcc4d0d29ae57a90051
JackDS <manojit.dhar.jack@gmail.com>
2025-02-28 16:06:15 +0530

    added bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Weather")),
-        body: Center(child: Text("Hello")),
-      ),
+      home: HomeScreen(),
     );
   }
 }
